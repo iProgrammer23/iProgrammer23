10c5793cf48a5e6f8f0d0737a5c39f81cb3faaea
Jasur <jasurolimjonov23@mail.ru>
2023-01-21 15:47:05 +0500

    Hello guys

---
+++ app.js
@@ -0,0 +1,116 @@
+'use strict'
+
+
+// console.log("Hello World");
+
+// let user = 'john'
+//    ,age = 32
+//    ,message = "Hello"
+
+// console.log(user);
+
+// num = 5
+
+// console.log(num);
+
+// let admin;
+// let name1 = 'John'
+
+// admin =  name1
+
+// alert(admin);
+
+// let enterName = prompt('What is your name: ')
+
+// alert(`hello ${enterName}`)
+
+"10"
+// nan
+// false
+// '2'
+"6"
+"9px"
+"$9"
+// nan
+// nan
+"-4"
+// nan
+// nan
+
+
+// let string = prompt('What is the "official" name of JavaScript');
+
+// if(string == "ECMAScript") {
+//     alert('Right')
+// } else {
+//     alert('You do not know? ECMAScript!')
+// }
+
+// Question mark operator 
+
+// let message = (string == "ECMAScript") ? '"Right"' : "You don't know? ECMAScript";
+
+// alert(message)
+
+// let message = 
+// (login == "Employee") ? message = 'Hello':
+// (login == 'Director') ? message = 'Greetings': 
+// (login == '') ? message = 'No login': message = ''; 
+
+
+// let age = 90;
+
+// if(age >= 14 && age <= 90) {
+//     console.log(true);
+// }   else {
+//     console.log(false);
+// }
+
+// let login = prompt("Whar is your login")
+
+// if (login == "Admin") {
+//     let password = prompt("Enter your Password")
+
+//     if (password == "TheMaster") {
+//         alert("Welcome")
+//     } else if (password == '' || password == null) {
+//         alert("Canceled")
+//     } else {
+//         alert("Wrong Password")
+//     }
+// }else if (login == '' || login == null) {
+//     alert("Canceled")
+// }else {
+//     alert("I don't know you")
+// }
+
+
+// for(let i = 2; i < 10; i++) {
+//     if(i % 2 == 1) continue
+//     alert(i)
+// }
+
+// let i = 0;
+// while (i < 3) {
+//     alert(`number ${i}!`);
+//     i++
+// }
+
+// let num;
+// do {
+//     num = +prompt("Enter a number greater than 100")
+// } while (num <= 100);
+
+// let a = +prompt('a?', '')
+// switch (a) {
+//     case a == 0:
+//         alert(0)
+//         break;
+//     case a == 1:
+//         alert(1)
+//         break
+//     case a == 2:
+//     case a == 3:
+//         alert('2,3');
+//         break;
+// }
